d220b8280c17568de616e308f25d68fdc2949f4b
Chien Shun Liu <chienshunliu@Zak-Notebook.local>
2026-02-14 02:02:09 +0800

    fix: configure database to support production persistence

--- config/settings.py
+++ config/settings.py
@@ -103,12 +103,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Local development fallback: use SQLite
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
